c6a09abf850852dfce1564dd712267e317ec4199
Igor Danilčenko <igor.danilcenko@doktor.se>
2026-01-07 18:49:34 +0100

    feat(minotari): Complete node setup and wallet integration
      Add node list assets, migration case 55, settings store integration,
      HTTPS URI parsing, build generation, and wallet keys display support.

--- lib/view_model/wallet_keys_view_model.dart
+++ lib/view_model/wallet_keys_view_model.dart
@@ -185,6 +185,9 @@ abstract class WalletKeysViewModelBase with Store {
         ]);
         break;
       case WalletType.minotari:
+        // Minotari uses BIP39 24-word mnemonic
+        // Seed will be displayed via the seed property once getMnemonic() FFI is implemented
+        break;
       case WalletType.none:
       case WalletType.haven:
         break;
@@ -271,6 +274,8 @@ abstract class WalletKeysViewModelBase with Store {
         return 'decred-wallet';
       case WalletType.dogecoin:
         return 'dogecoin-wallet';
+      case WalletType.minotari:
+        return 'minotari-wallet';
       default:
         throw Exception('Unexpected wallet type: ${_wallet.type.toString()}');
     }
