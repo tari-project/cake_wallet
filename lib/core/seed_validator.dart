ba90736d22505f96c284253e77a19f5907fb28f9
Igor Danilčenko <ihar.danilchanka@gmail.com>
2026-01-14 10:24:34 +0100

    feat(minotari): Implement scanner-based sync and transaction handling, update integration status (#5)

--- lib/core/seed_validator.dart
+++ lib/core/seed_validator.dart
@@ -4,7 +4,6 @@ import 'package:cake_wallet/bitcoin/bitcoin.dart';
 import 'package:cake_wallet/core/validator.dart';
 import 'package:cake_wallet/entities/mnemonic_item.dart';
 import 'package:cake_wallet/ethereum/ethereum.dart';
-import 'package:cake_wallet/minotari/minotari.dart';
 import 'package:cake_wallet/monero/monero.dart';
 import 'package:cake_wallet/nano/nano.dart';
 import 'package:cake_wallet/polygon/polygon.dart';
@@ -60,7 +59,7 @@ class SeedValidator extends Validator<MnemonicItem> {
       case WalletType.arbitrum:
         return arbitrum!.getArbitrumWordList(language);
       case WalletType.minotari:
-        return minotari!.getMinotariWordList(language);
+        return getBitcoinWordList(language);
       case WalletType.none:
       case WalletType.haven:
         return [];
